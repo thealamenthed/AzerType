013b56ed0183cf4d1ea4ad45ca23fddd89ae2f50
thealamenthed <dalila.boualemfr@gmail.com>
2024-11-19 15:13:25 +0100

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,15 @@
+/*********************************************************************************
+ *
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ *
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette'];
+const listePhrases = [
+  'La vie, l’univers et le reste',
+  'Pas de panique',
+  'Merci pour le poisson',
+];
